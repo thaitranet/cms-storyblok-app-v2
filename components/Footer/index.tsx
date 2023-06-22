20c27c33a4701d3f1c9aa35589ccc8a53fce3203
Thai Tran <thaitranet@gmail.com>
2023-06-22 10:24:51 +1000

    Update github link in footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -17,7 +17,7 @@ const Footer = () => {
                             Read Documentation
                         </a> 
                         <a
-                            href={`https://github.com/vercel/next.js/tree/canary/examples/${EXAMPLE_PATH}`}
+                            href={`https://github.com/thaitranet/cms-storyblok-app-v2`}
                             className="mx-3 font-bold hover:underline"
                         >
                             View on GitHub
